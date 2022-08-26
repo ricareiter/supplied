3a7fda3e05644b8fd0f0076062eb1e3a02d334dd
Ricardo Reiter <ricardoreiterr@gmail.com>
2022-08-26 10:10:41 -0300

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,10 @@
 class ProductsController < ApplicationController
+  before_action :get_client
   before_action :set_product, only: %i[ show edit update destroy ]
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = @client.products
   end
 
   # GET /products/1 or /products/1.json
@@ -12,7 +13,7 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    @product = Product.new
+    @product = @client.products.build
   end
 
   # GET /products/1/edit
@@ -21,12 +22,11 @@ class ProductsController < ApplicationController
 
   # POST /products or /products.json
   def create
-    @client = Client.find(params[:client_id])
-    @product = @client.products.new(product_params)
+    @product = @client.products.build(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to client_products_url(@product), notice: "Product was successfully created." }
+        format.html { redirect_to client_path(@client), notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
+        format.html { redirect_to client_product_path(@client), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,20 +53,24 @@ class ProductsController < ApplicationController
     @product.destroy
 
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.html { redirect_to client_path(@client), notice: "Product was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
 
+    def get_client
+      @client = Client.find(params[:client_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find(params[:id])
+      @product = @client.products.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:product_name, :quantity, :price, :client_id)
+      params.require(:product).permit(:product_name, :quantity, :price)
     end
 end
